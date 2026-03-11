3ea638331786498baa294697dab1a5dbfbbf321e
James Schulz <jamesschulzm@gmail.com>
2026-03-11 18:41:53 -0300

    [ADD] Conhecendo o useRef

--- src/components/Dialog/index.jsx
+++ src/components/Dialog/index.jsx
@@ -1,22 +1,25 @@
+import { useRef } from "react";
 import "./dialog.style.css";
 
 export function Dialog() {
   // Não utilizar!!!
   // const dialog = document.querySelector("dialog");
 
+  const dialogRef = useRef(null);
+
   // "Show the dialog" button opens the dialog modally
   const openDialog = () => {
-    dialog.showModal();
+    dialogRef.current.showModal();
   };
 
   // "Close" button closes the dialog
   const closeDialog = () => {
-    dialog.close();
+    dialogRef.current.close();
   };
 
   return (
     <>
-      <dialog>
+      <dialog ref={dialogRef}>
         <button autoFocus onClick={closeDialog}>
           Close
         </button>
